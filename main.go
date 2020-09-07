adc2a86491c64f50bf90290180a12cd5ea68e9d5
Blake Sager <blake.sager@activeprospect.com>
2020-09-07 11:49:01 -0500

    Finish chapter 7

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"lenslocked.com/views"
+)
+
+var homeView *views.View
+var contactView *views.View
+var faqView *views.View
 
 func main() {
-	fmt.Println("Hello")
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", pageHandlerFactory(homeView))
+	r.HandleFunc("/contact", pageHandlerFactory(contactView))
+	r.HandleFunc("/faq", pageHandlerFactory(faqView))
+
+	http.ListenAndServe(":3000", r)
+}
+
+func pageHandlerFactory(v *views.View) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		err := v.Render(w, nil)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
---
+++ views/view.go
@@ -0,0 +1,46 @@
+package views
+
+import (
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
+
+// NewView returns a view with the footer layout appended
+func NewView(layout string, files ...string) *View {
+	files = append(files, layoutFiles()...)
+
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		panic(err)
+	}
+
+	return &View{
+		Template: t,
+		Layout:   layout,
+	}
+}
+
+// View contains one property, Template, which is a pointer to a template
+type View struct {
+	Template *template.Template
+	Layout   string
+}
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
+
+// Render renders the template attached to a Vew
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
